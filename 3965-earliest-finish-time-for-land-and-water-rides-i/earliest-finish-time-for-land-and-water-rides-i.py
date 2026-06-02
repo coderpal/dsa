a9744b0329349095ee1e0915992041cbb61dbc02
Rahul Pal <rahulpal.aiml@gmail.com>
2026-06-02 13:14:22 +0530

    Time: 13 ms (58.14%) | Memory: 19.2 MB (86.05%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        
+        def getMinFinishTime(start1: List[int], dur1: List[int], start2: List[int], dur2: List[int]) -> int:
+
+            minEnd = min(s + d for s, d in zip(start1, dur1))
+
+            return min(max(s, minEnd) + d for s, d in zip(start2, dur2))
+
+        landFirst = getMinFinishTime(landStartTime, landDuration, waterStartTime, waterDuration)
+
+        waterFirst = getMinFinishTime(waterStartTime, waterDuration, landStartTime, landDuration)
+
+        return min(landFirst, waterFirst)
